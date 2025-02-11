ee95dfcba1aeb9fbfe590e0aafdc42ae93508976
mulukentessema12@gmail.com <mulukentessema12@gmail.com>
2025-02-11 10:23:18 -0500

    gitignore

--- netflix-clone/src/utlis/requests.jsx
+++ netflix-clone/src/utlis/requests.jsx
@@ -1,4 +1,4 @@
-const API_KEY ="45bf211dba8c37ecca74385c02799cec";
+const API_KEY =process.env.REACT_APP_API_KEY ;
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
